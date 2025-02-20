28ee5091aec2e834174fafc9d542bea3cfeb6907
Minh Ngoc Doan <minhngocdoan3112@gmail.com>
2025-02-20 21:05:23 +0700

    Connect BE with Postgres database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,23 @@
 import express from "express";
 import cors from 'cors';
+import pool from './db'
+require('dotenv').config();
 
 const app = express();
-app.use(cors());
 const PORT = 5000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()'); 
+    console.log("✅ Successfully connected to PostgreSQL!");
+    res.json({ message: "PostgreSQL connected!", time: result.rows[0] });
+  } catch (error) {
+    console.error("Database connection error:", error);
+    res.status(500).json({ error: (error as Error).message });
+  }
+});
 
 app.get("/", (req, res) => {
   res.json("Hello from the backend!");
